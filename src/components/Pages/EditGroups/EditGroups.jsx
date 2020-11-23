d2cadc323b59e2f818946df05af207d577550030
OnAlex911 <onalex911@gmail.com>
2020-11-23 23:57:16 +0400

    Container: вызывает объектный компонент, определяющий путь и вычисляющий нужный заголовок. Но работает ОЧЕНЬ медленно!

--- src/components/Pages/EditGroups/EditGroups.jsx
+++ src/components/Pages/EditGroups/EditGroups.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../../../css/sb-admin-2.css'
 
+
 const EditGroups= (props) => {
     return (
         <div className='row'>
 
             <h6>Editing of Groups</h6>
+
+
 
         </div>
     )
